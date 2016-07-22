7800f4757c42efd85a6cb9d5efd34715ef9c3330
Daniel Avila <dan.avila7@gmail.com>
2016-07-22 01:59:30 -0300

    dash2

--- AG_beta6/Controllers/HomeController.cs
+++ AG_beta6/Controllers/HomeController.cs
@@ -34,11 +34,16 @@ namespace AG_beta6.Controllers
         [AllowAnonymous]
         public ActionResult Dashboard()
         {
-            Dictionary<string, int> resultConductor = db.Viaje.GroupBy(z => z.Conductor).ToDictionary(x => x.Key.Nombre, x => x.Key.Viaje.Count);
-            Dictionary<string, int> resultReservas = db.Reserva.GroupBy(z => z.Cliente).ToDictionary(x => x.Key.Nombre + "(" + x.Key.Empresa.Nombre +")" , x => x.Key.Reserva.Count);
-
+            Dictionary<string, int> resultConductor = db.Viaje.Where(r => r.FechaInicio.Day == DateTime.Now.Day && r.FechaInicio.Month == DateTime.Now.Month && r.FechaInicio.Year == DateTime.Now.Year).GroupBy(z => z.Conductor).ToDictionary(x => x.Key.Nombre, x => x.Key.Viaje.Count);
+            Dictionary<string, int> resultReservas = db.Reserva.Where(r => r.Fecha_viaje.Value.Day == DateTime.Now.Day && r.Fecha_viaje.Value.Month == DateTime.Now.Month && r.Fecha_viaje.Value.Year == DateTime.Now.Year).GroupBy(z => z.Cliente).ToDictionary(x => x.Key.Nombre + "(" + x.Key.Empresa.Nombre + ")", x => x.Key.Reserva.Count);
+            Dictionary<string, int> resultReservasPendientes = db.Reserva.Where(r => r.Pendiente == 1).GroupBy(z => z.Cliente).ToDictionary(x => x.Key.Nombre + "(" + x.Key.Empresa.Nombre + ")", x => x.Key.Reserva.Count);
+            Dictionary<string, int> resultViajes = db.Viaje.Where(v => v.FechaInicio.Month == DateTime.Now.Month && v.IdEstado == 3).GroupBy(z => z.Cliente.Empresa).ToDictionary(x => x.Key.Nombre, x => x.Key.Cliente.First().Viaje.Count);
+            Dictionary<string, int> resultViajesDia = db.Viaje.Where(r => r.FechaInicio.Day == DateTime.Now.Day && r.FechaInicio.Month == DateTime.Now.Month && r.FechaInicio.Year == DateTime.Now.Year && r.IdEstado == 2).GroupBy(z => z.Cliente).ToDictionary(x => x.Key.Nombre + "(" + x.Key.Empresa.Nombre + ")", x => x.Key.Reserva.Count);
             ViewBag.resultConductor = resultConductor;
             ViewBag.resultReservas = resultReservas;
+            ViewBag.resultViajes = resultViajes;
+            ViewBag.resultViajesDia = resultViajesDia;
+            ViewBag.resultReservasPendientes = resultReservasPendientes;
             return View();
         }
 
